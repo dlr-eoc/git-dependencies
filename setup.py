5f8e9fc657892a46d0b4744bd895877346c1ed94
Nico Mandery <nico.mandery@dlr.de>
2016-02-16 09:41:16 +0100

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup
 
 setup(name='git-dependencies',
-    version='0.1',
+    version='0.1.0',
     description='Fetch external dependencies from their git repositories and integrate them in the working tree.',
     author='Nico Mandery',
     author_email='nico.mandery@dlr.de',
     scripts=['git-dependencies'],
-    install_requires=['PyYAML>=3.0', 'pexpect>=3.1']
+    install_requires=['PyYAML>=3.0', 'pexpect>=3.1'],
+    url="http://git.ukis.eoc.dlr.de/projects/ADMIN/repos/git-dependencies/browse"
 )
